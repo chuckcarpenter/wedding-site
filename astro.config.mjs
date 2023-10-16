7908453d1d39b9cf92e7477a0dfb16c94b469c1e
Chuck Carpenter <chuck.carpenter@me.com>
2023-10-16 14:58:12 -0700

    👷 Add astro and convert files for site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
 
-import image from "@astrojs/image";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image(), UnoCSS()],
+  integrations: [UnoCSS()],
 });
